04ea112537dafb5e69d4b8c40fdc5927161dd0cb
AP Ljungquist <ap@ljungquist.eu>
2023-07-10 21:36:52 +0200

    Factor out handlers to separate module
    The package really does two things:
    1. It provides handlers that color the output nicely, and
    2. It provides configuration with nice defaults that can be overridden
       by the end user through environment variables.
    
    I think separating the two makes the code easier to read.

--- src/env_logger/__init__.py
+++ src/env_logger/__init__.py
@@ -1,140 +1,15 @@
 from __future__ import annotations
 
 import argparse
-import copy
-import json
 import logging
 import os
 import sys
 from typing import TYPE_CHECKING
 
+from . import _handlers
+
 if TYPE_CHECKING:
-    from typing import Optional, List, Callable, Any, Dict, Iterable, Self, Tuple
-
-import colorama
-
-logger = logging.getLogger(__name__)
-
-
-class ColorMap:
-    @classmethod
-    def dim_to_bright(cls) -> Self:
-        return cls(
-            [
-                (logging.DEBUG, colorama.Style.DIM),
-                (logging.INFO, colorama.Style.NORMAL),
-                (logging.WARNING, colorama.Style.NORMAL + colorama.Fore.YELLOW),
-                (logging.ERROR, colorama.Style.NORMAL + colorama.Fore.RED),
-                (logging.CRITICAL, colorama.Style.BRIGHT + colorama.Fore.RED),
-            ]
-        )
-
-    @classmethod
-    def dim_to_back(cls) -> Self:
-        return cls(
-            [
-                (logging.DEBUG, colorama.Style.DIM),
-                (logging.INFO, colorama.Style.BRIGHT),
-                (logging.WARNING, colorama.Style.BRIGHT + colorama.Fore.YELLOW),
-                (logging.ERROR, colorama.Style.BRIGHT + colorama.Fore.RED),
-                (
-                    logging.CRITICAL,
-                    colorama.Style.BRIGHT + colorama.Fore.WHITE + colorama.Back.RED,
-                ),
-            ]
-        )
-
-    @classmethod
-    def dim_or_normal(cls) -> Self:
-        return cls(
-            [
-                (logging.DEBUG, colorama.Style.DIM),
-                (logging.CRITICAL, colorama.Style.NORMAL),
-            ]
-        )
-
-    def __init__(self, styles: Iterable[Tuple[int, str]]) -> None:
-        self._styles = list(styles)
-
-    def color(self, level: int) -> str:
-        for l, s in self._styles:
-            if level <= l:
-                return s
-        return ""
-
-    def colored(self, level: int, text: str) -> str:
-        return self.color(level) + text + colorama.Style.RESET_ALL
-
-
-class Handler(logging.StreamHandler):
-    def __init__(self, *args, **kwargs) -> None:
-        self._style_output = kwargs.pop("style_output", True)
-        self._color_map = ColorMap.dim_to_bright()
-        super().__init__(*args, **kwargs)
-
-    def format(self, record: logging.LogRecord) -> str:
-        default = super().format(record)
-        # TODO: Consider making configurable
-        escaped = json.dumps(default)[1:-1]
-        # TODO: Consider styling unnamed levels
-        colored = (
-            self._color_map.colored(record.levelno, escaped)
-            if self._style_output
-            else escaped
-        )
-        return colored
-
-
-def _without_exc_info(record: logging.LogRecord) -> logging.LogRecord:
-    record = copy.copy(record)
-    record.exc_info = None
-    return record
-
-
-class SparseColorFormatter(logging.Formatter):
-    def __init__(self, *args, **kwargs) -> None:
-        fmt: str = kwargs.pop("fmt")
-        left, middle, right = fmt.rpartition("%(levelname)s")
-
-        kwargs["fmt"] = left  # makes mypy happy
-        self._left = logging.Formatter(*args, **kwargs) if left else None
-        self._middle = middle
-        kwargs["fmt"] = right
-        self._right = logging.Formatter(*args, **kwargs)
-
-        self._level_color_map = ColorMap.dim_to_back()
-        self._other_color_map = ColorMap.dim_or_normal()
-        super().__init__(*args, **kwargs)
-
-    def format(self, record: logging.LogRecord) -> str:
-        level_style = self._level_color_map.color(record.levelno)
-        other_style = self._other_color_map.color(record.levelno)
-        record_without_exc_info = _without_exc_info(record)
-        # TODO: Consider optimizing
-        return str(
-            (
-                other_style
-                + (
-                    json.dumps(self._left.format(record_without_exc_info))[1:-1]
-                    if self._left
-                    else ""
-                )
-                + colorama.Style.RESET_ALL
-                + level_style
-                + (record.levelname if self._middle else "")
-                + colorama.Style.RESET_ALL
-                + other_style
-                + (json.dumps(self._right.format(record))[1:-1])
-                + colorama.Style.RESET_ALL
-            )
-        )
-
-
-class SparseColorHandler(logging.StreamHandler):
-    def setFormatter(self, fmt: Optional[logging.Formatter]) -> None:
-        if fmt is not None:
-            fmt = SparseColorFormatter(fmt=fmt._fmt)
-        super().setFormatter(fmt)
+    from typing import Optional, List, Callable, Any, Dict
 
 
 def _resolve(
@@ -185,7 +60,7 @@ def _valid_handlers(text: Optional[str]) -> Optional[List[logging.Handler]]:
                 f"Invalid log handler: {text} (install rich to enable this handler)"
             ) from e
     if text == "sparse":
-        return [SparseColorHandler()]
+        return [_handlers.SparseColorHandler()]
     raise ValueError(f"Invalid log handler: {text}")
 
 
@@ -218,7 +93,7 @@ def configure(**kwargs) -> None:
         kwargs,
         "handlers",
         lambda: _valid_handlers(os.environ.get("LOG_HANDLER")),
-        lambda: [Handler(style_output=_style_output())],
+        lambda: [_handlers.Handler(style_output=_style_output())],
     )
     logging.basicConfig(**kwargs)
 
@@ -239,7 +114,7 @@ def _log_samples(logger: logging.Logger) -> None:
 
 
 def _demo() -> None:
-    _log_samples(logger)
+    _log_samples(logging.getLogger(__name__))
 
 
 def _parser() -> argparse.ArgumentParser:
--- tests/test_env_logger.py
+++ tests/test_env_logger.py
@@ -1,17 +1,18 @@
 import logging
 
 import env_logger
+from env_logger import _handlers
 
 
 def test_sparse_color_handler_accepts_missing_levelname() -> None:
-    handler = env_logger.SparseColorHandler()
+    handler = _handlers.SparseColorHandler()
     handler.setFormatter(logging.Formatter("%(message)s"))
     logger = logging.Logger("test")
     logger.addHandler(handler)
     env_logger._log_samples(logger)
 
 def test_sparse_color_handler_accepts_basic_format() -> None:
-    handler = env_logger.SparseColorHandler()
+    handler = _handlers.SparseColorHandler()
     handler.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
     logger = logging.Logger("test")
     logger.addHandler(handler)
